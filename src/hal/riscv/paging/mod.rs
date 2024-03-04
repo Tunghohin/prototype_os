ede18569287420fe88472b1c9276233681d36aa8
Tunghohin <houhin0809@gmail.com>
2024-03-04 11:28:53 +0800

    Update minor fix

--- src/hal/riscv/paging/mod.rs
+++ src/hal/riscv/paging/mod.rs
@@ -1,9 +1,8 @@
-use core::arch::asm;
 use riscv::register::satp;
 
 pub mod entry;
 
 pub fn activate_virt_mem(token: usize) {
-    let satp_token = 8usize << 60 | token;
+    let satp_token = 0b1000 << 60 | token;
     satp::write(satp_token);
 }
